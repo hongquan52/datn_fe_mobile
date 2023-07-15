92264783c19ddaf5e117166ac7f5c602f4a48e2c
hongquan52 <nguyenhongquan9a5@gmail.com>
2023-07-15 14:50:11 +0700

    fix mobile

--- consts/BaseURL.js
+++ consts/BaseURL.js
@@ -2,7 +2,7 @@
 // export const BaseURL = 'http://192.168.100.24:8080';
 
 // 4G VIETTEL
-// export const BaseURL = 'http://172.20.10.2:8080';
+export const BaseURL = 'http://172.16.32.28:8080';
 
 // Wifi ở quê
 // export const BaseURL = 'http://192.168.1.3:8080';
@@ -14,7 +14,7 @@
 // export const BaseURL = 'http://192.168.1.166:8080';
 
 // tro 17A
-export const BaseURL = 'http://192.168.1.216:8080';
+// export const BaseURL = 'http://192.168.1.216:8080';
 
 // chung cư ngài
 // export const BaseURL = 'http://192.168.1.216:8080';
